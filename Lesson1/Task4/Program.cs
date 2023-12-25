081e78a1deb368ee3ebfac3908487a2a7b78159c
Nevada <rjkey171196@gmail.com>
2023-12-25 16:56:15 +1000

    Add lesson1:task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача с 5-ю гирями
+
+int a = 8;
+int b = 10;
+int c = 6;
+int d = 16;
+int e = 24;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
