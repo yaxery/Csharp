90c7851d5678c08177ec648fd54c60ae042063e5
Nevada <rjkey171196@gmail.com>
2023-12-27 14:15:18 +1000

    Add Sem1 programm

---
+++ Seminar1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Первый семинар по теме Csharp
+// Пишем программу, которая принимает на входе 2 целых числа, и проверяет, является ли первое квадратом второго.
+
+
+//int first_number = 25;
+//int second_number = 4;
+
+//if(second_number*second_number == first_number){
+  //  Console.Write("First num is a quater of second num");
+//}
+//else{
+//    Console.Write("First num isnt a quater of second num");
+//}
+
+
