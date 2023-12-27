807e1f8ccbad3d37cd4d7259ea41acdb4d37c636
Nevada <rjkey171196@gmail.com>
2023-12-27 22:14:21 +1000

    Add third programm

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -22,3 +22,17 @@
 //     Console.Write($"{num2} ");
 //     num2++;
 // }
+
+
+// Программа, принимающая на входе трёхзначное число, на выходе показывает сумму первой и последней цифры этого числа
+
+int num = 199;
+if(num >= 100 && num <= 999){
+    int num1 = num / 100;
+    int num2 = num % 10;
+    int sum = num1 + num2;
+    Console.Write($"Sum of first and last digit in numb {num} is {sum}");
+}
+else{
+    Console.Write($"Your numb isnt 3digit");
+}
